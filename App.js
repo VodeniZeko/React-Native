938a4a6b5f172993c03dd3706ceaa8951d33d8b2
VodeniZeko <salexy.es@gmail.com>
2020-04-06 22:03:02 -0700

    first push

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
-import FriendsList from "./src/screens/FriendsList";
-import ComponentScreen from "./src/screens/ComponentScreen";
-import ImageScreen from "./src/screens/ImageScreen";
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Components: ComponentScreen,
-    List: FriendsList,
-    Image: ImageScreen
+    Home: HomeScreen
   },
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "testin app"
+      title: "App"
     }
   }
 );
